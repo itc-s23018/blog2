8ce89b41d84017a9bc5f4c7b402f8ad0c0fc99b4
itc-s23018 <s23018@std.it-college.ac.jp>
2024-02-06 10:29:41 +0000

    9.8  ページネーションによる遷移で画像のブラー表示を機能させる
    <Image /> 属性にKey属性を追加し、ブラー表示を機能させた

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -42,6 +42,7 @@ const Post = ({
         <PostHeader title={title} subtitle='Blog Article' publish={publish} />
         <figure>
           <Image
+            key={eyecatch.url}
             src={eyecatch.url}
             alt=''
             layout='responsive'
