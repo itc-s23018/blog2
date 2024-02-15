d8453860f0e736b1f6dd0446f269a4103088a2d7
itc-s23018 <s23018@std.it-college.ac.jp>
2024-02-15 07:29:01 +0000

    10.7 useEffectを使い、Googleのアナリティクスを設定

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,43 @@
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
 import 'styles/globals.css'
 import Layout from 'components/layout'
+import Script from 'next/script'
+import * as gtag from 'lib/gtag'
 
 import '@fortawesome/fontawesome-svg-core/styles.css'
 import { config } from '@fortawesome/fontawesome-svg-core'
 config.autoAddCss = false
 
 const MyApp = ({ Component, pageProps }) => {
+  const router = useRouter()
+  useEffect(() => {
+    const handleRouteChange = url => {
+      gtag.pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Script
+        async
+        src={`https://www.googletamanager.com/gtag/js?id=${gtag.GA_MEASUREMENR_ID}`}
+      />
+      <Script>
+        {`
+        window.dataLayer = window.dataLayer || []; function gtag()
+        {dataLayer.push(arguments)}
+        gtag('js', new Date()); gtag('config', '${gtag.GA_MEASUREMENT_ID}');
+    `}
+      </Script>
+
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   )
 }
 export default MyApp
