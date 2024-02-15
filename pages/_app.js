2aa270da520e2343996ac253644cd234da6d389a
itc-s23018 <s23018@std.it-college.ac.jp>
2024-02-15 06:19:37 +0000

    10.7をやり直すためリセット

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,34 @@
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
 import 'styles/globals.css'
 import Layout from 'components/layout'
+import Script from 'next/script'
+import * as gtag from 'lib/gtag'
 
 import '@fortawesome/fontawesome-svg-core/styles.css'
 import { config } from '@fortawesome/fontawesome-svg-core'
 config.autoAddCss = false
 
 const MyApp = ({ Component, pageProps }) => {
+  const router = useRouter()
+  useEffect(() => {
+    const handleRouteChange = url => {
+      gtag.pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+        }}
+      />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   )
 }
+
 export default MyApp
